6ccff84d646f2eb5b739e844eb857a35c8a9ae08
Pierre-Yves <py.touzain@gmail.com>
2015-05-14 16:52:20 +0200

    Initial commit

---
+++ Sugar Mole/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 14/05/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sugar Mole/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 14/05/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
