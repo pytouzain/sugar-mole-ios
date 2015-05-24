2cb8866a2a0f3f6c8af28c9b434ac515f66566de
Pierre-Yves <py.touzain@gmail.com>
2015-05-24 02:18:48 +0200

    Vocal control view controller added

---
+++ Sugar Mole/VocalControlViewController.h
@@ -0,0 +1,13 @@
+//
+//  VocalControlViewController.h
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 24/05/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VocalControlViewController : UIViewController
+
+@end
---
+++ Sugar Mole/VocalControlViewController.m
@@ -0,0 +1,32 @@
+//
+//  VocalControlViewController.m
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 24/05/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import "VocalControlViewController.h"
+
+@interface VocalControlViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *instructionLabel;
+@property (weak, nonatomic) IBOutlet UIButton *vocalControlButton;
+
+@end
+
+@implementation VocalControlViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _vocalControlButton.layer.masksToBounds = YES;
+    _vocalControlButton.layer.cornerRadius = _vocalControlButton.frame.size.height / 2.f;
+    _vocalControlButton.layer.borderColor = [[UIColor whiteColor] CGColor];
+    _vocalControlButton.layer.borderWidth = 8.f;
+}
+
+- (IBAction)backToHomeView:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
