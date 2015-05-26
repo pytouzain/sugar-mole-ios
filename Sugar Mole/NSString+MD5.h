e094f0dfc08402e89c26efaa31cb32e227004a4e
Pierre-Yves <py.touzain@gmail.com>
2015-05-27 01:03:10 +0200

    MD5 category added to NSString

---
+++ Sugar Mole/NSString+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSString+MD5.h
+//  Spicer
+//
+//  Created by Pierre-Yves Touzain on 02/05/2015.
+//  Copyright (c) 2015 Spicer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+
+- (NSString *)md5;
+
+@end
