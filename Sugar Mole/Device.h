546c6fd24670b7a8b75ad44245278bb07a63d29a
Pierre-Yves <py.touzain@gmail.com>
2015-05-28 00:01:32 +0200

    Methods declaration added to httprequestmanager header file

---
+++ Sugar Mole/Device.h
@@ -0,0 +1,13 @@
+//
+//  Device.h
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 27/05/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import "RLMObject.h"
+
+@interface Device : RLMObject
+
+@end
---
+++ Sugar Mole/Device.m
@@ -0,0 +1,13 @@
+//
+//  Device.m
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 27/05/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import "Device.h"
+
+@implementation Device
+
+@end
