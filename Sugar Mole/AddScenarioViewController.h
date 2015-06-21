6c96a725e8c2df301e1a8d9a9026be9ed07408d2
Pierre-Yves Touzain <py.touzain@gmail.com>
2015-06-21 02:37:31 +0200

    Add scenario VC in progress

---
+++ Sugar Mole/AddScenarioViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddScenarioViewController.h
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 21/06/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import "ItemsTableViewController.h"
+
+@interface AddScenarioViewController : ItemsTableViewController
+
+@end
--- Sugar Mole/AddScenarioViewController.m
+++ Sugar Mole/AddScenarioViewController.m
@@ -1,27 +1,26 @@
 //
-//  NewScenarioViewController.m
+//  AddScenarioViewController.m
 //  Sugar Mole
 //
-//  Created by Pierre-Yves Touzain on 14/05/2015.
+//  Created by Pierre-Yves Touzain on 21/06/2015.
 //  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
 //
 
-#import "NewScenarioViewController.h"
+#import "AddScenarioViewController.h"
 
-@interface NewScenarioViewController ()
+@interface AddScenarioViewController ()
 
 @end
 
-@implementation NewScenarioViewController
+@implementation AddScenarioViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)back:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
 /*
