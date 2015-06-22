ca37a99bee27cb895d65ba6cdabfb5cb6284cd8c
Pierre-Yves Touzain <py.touzain@gmail.com>
2015-06-23 01:47:33 +0200

    AddScenarioDataModel added

---
+++ Sugar Mole/AddScenarioDataModel.h
@@ -0,0 +1,13 @@
+//
+//  AddScenarioDataModel.h
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 23/06/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AddScenarioDataModel : NSObject
+
+@end
---
+++ Sugar Mole/AddScenarioDataModel.m
@@ -0,0 +1,13 @@
+//
+//  AddScenarioDataModel.m
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 23/06/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import "AddScenarioDataModel.h"
+
+@implementation AddScenarioDataModel
+
+@end
