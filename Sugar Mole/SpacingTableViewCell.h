0dc07a7785857d64d57f017510fd2fe80033e254
Pierre-Yves Touzain <py.touzain@gmail.com>
2015-06-23 00:52:53 +0200

    SpacingTableViewCell added

---
+++ Sugar Mole/SpacingTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SpacingTableViewCell.h
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 23/06/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpacingTableViewCell : UITableViewCell
+
+@end
---
+++ Sugar Mole/SpacingTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SpacingTableViewCell.m
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 23/06/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import "SpacingTableViewCell.h"
+
+@implementation SpacingTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
