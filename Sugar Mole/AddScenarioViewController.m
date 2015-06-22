ae31106a69202ec040426359ecd545b5f16d9db6
Pierre-Yves Touzain <py.touzain@gmail.com>
2015-06-23 01:48:34 +0200

    AddScenarioVC adapted to the model

--- Sugar Mole/AddScenarioViewController.m
+++ Sugar Mole/AddScenarioViewController.m
@@ -7,12 +7,23 @@
 //
 
 #import "AddScenarioViewController.h"
+#import "AddScenarioDataModel.h"
 
 @interface AddScenarioViewController ()
+
+@property (nonatomic, strong) AddScenarioDataModel *model;
 
 @end
 
 @implementation AddScenarioViewController
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        self.model = [[AddScenarioDataModel alloc] init];
+    }
+    return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -21,6 +32,32 @@
 
 - (IBAction)back:(id)sender {
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (section == 0) {
+        return NUMBER_OF_ROW([self.model.triggers count]);
+    }
+    return NUMBER_OF_ROW([self.model.actions count]);
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 50.;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    //create AddScenario HeaderView
+    UIView *headerView = [[UIView alloc] init];
+    headerView.backgroundColor = [UIColor clearColor];
+    return headerView;
 }
 
 /*
