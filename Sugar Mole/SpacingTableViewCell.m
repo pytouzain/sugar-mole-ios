c6df241b04bd7209037351c86cd6061c9579bbbf
Pierre-Yves Touzain <py.touzain@gmail.com>
2015-06-23 01:49:30 +0200

    SpacingTableViewCell code removing

--- Sugar Mole/SpacingTableViewCell.m
+++ Sugar Mole/SpacingTableViewCell.m
@@ -11,13 +11,6 @@
 @implementation SpacingTableViewCell
 
 - (void)awakeFromNib {
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 @end
