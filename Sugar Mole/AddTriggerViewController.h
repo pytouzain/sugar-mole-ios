5eebfb7f408d6f397388f3e6faaf7e4fece4395c
Pierre-Yves Touzain <py.touzain@gmail.com>
2015-06-24 01:28:06 +0200

    Add Trigger view designed

---
+++ Sugar Mole/AddTriggerViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddTriggerViewController.h
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 24/06/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddTriggerViewController : UIViewController
+
+@end
---
+++ Sugar Mole/AddTriggerViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddTriggerViewController.m
+//  Sugar Mole
+//
+//  Created by Pierre-Yves Touzain on 24/06/2015.
+//  Copyright (c) 2015 Pierre-Yves Touzain. All rights reserved.
+//
+
+#import "AddTriggerViewController.h"
+
+@interface AddTriggerViewController ()
+
+@end
+
+@implementation AddTriggerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
