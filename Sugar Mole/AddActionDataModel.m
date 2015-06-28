95cdf1a944e55e36cffa59df493a1454e5426031
Pierre-Yves Touzain <py.touzain@gmail.com>
2015-06-29 01:04:19 +0200

    Add datas in AddActionDataModel

--- Sugar Mole/AddActionDataModel.m
+++ Sugar Mole/AddActionDataModel.m
@@ -17,16 +17,34 @@
 
 - (NSDictionary *)actions {
     return @{@"Security" : @[[[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Unlock-100"] descriptionItem:@"Open the front door"],
-                             [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Lock 2-100_white"] descriptionItem:@"Close the front door"]
+                             [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Lock 2-100_white"] descriptionItem:@"Close the front door"],
+                             [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Unlock-100"] descriptionItem:@"Open the back door"],
+                             [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Lock 2-100_white"] descriptionItem:@"Close the back door"],
+                             [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Unlock-100"] descriptionItem:@"Open the garage door"],
+                             [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Lock 2-100_white"] descriptionItem:@"Close the garage door"]
                              ],
-             @"Lamps" : @[[[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Idea-100"] descriptionItem:@"Turn on living room lamp"],
-                          [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Idea-100_switch_off"] descriptionItem:@"Turn off living room lamp"]
+             @"Lamps" : @[[[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Idea-100"] descriptionItem:@"Turn on the kids bedroom light"],
+                          [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Idea-100_switch_off"] descriptionItem:@"Turn off the kids bedroom light"],
+                          [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Idea-100"] descriptionItem:@"Turn on the parents bedroom light"],
+                          [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Idea-100_switch_off"] descriptionItem:@"Turn off the parents bedroom light"],
+                          [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Idea-100"] descriptionItem:@"Turn on the living room lamp"],
+                          [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Idea-100_switch_off"] descriptionItem:@"Turn off the living room lamp"],
+                          [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Idea-100"] descriptionItem:@"Turn on the kitchen light"],
+                          [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Idea-100_switch_off"] descriptionItem:@"Turn off the kitchen light"],
+                          [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Idea-100"] descriptionItem:@"Turn on the bathroom light"],
+                          [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Idea-100_switch_off"] descriptionItem:@"Turn off the bathroom light"]
                           ],
-             @"Air conditionning" : @[[[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Winter-100_fat"] descriptionItem:@"Turn on the bedroom AC"],
-                                      [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Winter-100_thin"] descriptionItem:@"Turn off the bedroom AC"]
+             @"Air conditionning" : @[[[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Winter-100_fat"] descriptionItem:@"Turn on the parents bedroom AC"],
+                                      [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Winter-100_thin"] descriptionItem:@"Turn off the parents bedroom AC"],
+                                      [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Winter-100_fat"] descriptionItem:@"Turn on the living room AC"],
+                                      [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Winter-100_thin"] descriptionItem:@"Turn off the living room AC"]
                                       ],
              @"Heater" : @[[[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Heating Room Filled-100"] descriptionItem:@"Turn on the kids bedroom heater"],
-                           [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Heating Room-100"] descriptionItem:@"Turn off the kids bedroom heater"]]
+                           [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Heating Room-100"] descriptionItem:@"Turn off the kids bedroom heater"],
+                           [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Heating Room Filled-100"] descriptionItem:@"Turn on the parents bedroom heater"],
+                           [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Heating Room-100"] descriptionItem:@"Turn off the parents bedroom heater"],
+                           [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Heating Room Filled-100"] descriptionItem:@"Turn on the living room heater"],
+                           [[ActionDataModel alloc] initWithImage:[UIImage imageNamed:@"Heating Room-100"] descriptionItem:@"Turn off the living room heater"]]
              };
 }
 
